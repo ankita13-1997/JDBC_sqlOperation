229a452944a64c3967030840095d3353a553cdd5
ankita13-1997 <parhiankita@gmail.com>
2021-03-13 01:16:51 +0530

    Added a welcome msg

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.test.mysql;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to the emplyee reteival of data");
+        
+    }
+}
